de45311e2a55bbc6a8d025dda6c9b0befefa152f
Ulsbold Enkhbold <ulsbold.e@gmail.com>
2021-10-06 22:12:50 +0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Ulsbold Enkhbold on 2021.10.06.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
